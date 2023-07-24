5903b26ba000a20ba1058b7e30cd7c9cdfebe8ee
Stephany Truscott <st.truscott@gmail.com>
2023-07-24 17:12:55 +0200

    Completed & Uploaded DWA5

--- DWA5/js/scripts.js
+++ DWA5/js/scripts.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("[data-form]");
-
+//User Stories - Starting program state
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
@@ -9,26 +9,25 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
 
   try {
+    //User Stories - Validation when values are missing
     if (divider == "" || dividend == "") {
       throw "Division not performed. Both values are required in inputs. Try again ";
     }
 
+    //User Stories - An invalid division should log an error in the console
     if (divider < 0) {
       throw "Division not performed. Invalid number provided. Try again";
-
-      //   console.error("Invalid number provided");
-      //   console.trace();
     }
 
+    //User Stories - Providing anything that is not a number should crash the program
     if (isNaN(dividend) || isNaN(divider)) {
       throw (document.body.innerHTML = `Something critical went wrong. Please reload the page.`);
-
-      //   console.error("Critical error: Non-numeric value provided");
-      //   console.trace();
     }
 
+    //User Stories - Dividing numbers result in a whole number & Dividing numbers result in a decimal number
     result.innerText = `${Math.floor(dividend / divider)}`;
   } catch (e) {
+    //Console log errors
     console.error(e);
     result.innerHTML = e;
   }
